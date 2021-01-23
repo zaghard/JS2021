d06dea3d0667f5fdcfda1613dc4842b2154aedc4
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-01-22 22:22:25 -0300

    blocos de codigo
    aula 25 da revisao js

---
+++ fundamentos/inicio/blocos.js
@@ -0,0 +1,12 @@
+//  BLOCOS DE CÓDIGO!!!
+{
+    console.log("PASSO 01");
+    console.log("PASSO 02");
+    console.log("PASSO 03");
+    
+}
+
+{
+    console.log("PASSO 04");
+    console.log("PASSO 05");
+}
