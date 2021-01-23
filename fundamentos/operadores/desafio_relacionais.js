573baf7401f529c60533fac9d8ffbeb6a7a3cbb7
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-01-23 20:11:04 -0300

    operadores de atribuição
    exercicio com operadores de atribuição

---
+++ fundamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,20 @@
+let a = 1;
+let b = 2;
+
+
+//Encontre 5 expressões que o resultado é TRUE.
+console.log(" Epressões com TRUE:")
+console.log(a < b);
+console.log(b > a);
+console.log(a != b);
+console.log(b !== a);
+console.log(a + a === b);
+console.log("")
+
+//Encontre 5 expressões que o resultado é FALSE.
+console.log(" Expressões com FALSE:");
+console.log(a > b);
+console.log(b < a);
+console.log(a == b);
+console.log(b === a);
+console.log(a + a !== b);
