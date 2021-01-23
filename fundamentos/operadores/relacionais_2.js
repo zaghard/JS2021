9f5f1705162d51d7fdd9a8300792b98863689728
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-01-23 19:51:14 -0300

    Update relacionais_2.js
    Comparativos de igualdade e diferença

---
+++ fundamentos/operadores/relacionais_2.js
@@ -0,0 +1,14 @@
+let a = 1;
+let b = "1";
+let c = "1";
+
+// Igual ou estritamente igual.
+console.log(a == b);
+console.log(a === b);
+console.log(b === c);
+
+// Diferente ou estritamente diferente.
+console.log(" Diferença:")
+console.log(a != b);
+console.log(a !== b);
+console.log(b != c);
