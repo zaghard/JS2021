c32169cadd0427d2addb54240ef135dc7833b4a7
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-01-28 15:45:10 -0300

    atividade do dia 28/01/21

---
+++ fundamentos/inicio/numeroscuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); //infinity
+console.log("10"/2);// o javascript faz a divisao
+console.log("show!"* 2);//not a number
+console.log(0.1 + 0.8);
+//console.log(10.toString());
+console.log((10.345).toFixed(2));
---
+++ fundamentos/inicio/variveis.js
@@ -0,0 +1,14 @@
+//Declaração de variaveis
+
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+console.log(a, b);
+
+const c = 5;
+// c = 50;
+
+console.log(a ,b ,c);
+
