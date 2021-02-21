b1409251fd4c20e05dd9d5b60aa8829aa9e9a334
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-21 15:39:18 -0300

    tipos de função

--- aulas_youtube_js/aula01/index.js
+++ aulas_youtube_js/aula01/index.js
@@ -1,4 +1,67 @@
-let idade = 5;
-console.log(idade);
-let altura = 180;
-console.log(altura);
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+
+// constante e variaveis
+
+// const valorIngressoAdulto = 20;
+// let valorIngressoAdulto = 20;
+// valorIngressoAdulto = 30;
+// console.log(valorIngressoAdulto);
+
+
+//Tipos primitivos
+// let nome = 'Mauricio'; //String
+// let idade = 40 ; //number literal
+// let aprovad = true; //boolean
+// let sobrenome = undefined; //Undefined
+// let corSelecionado = null //nulo
+
+
+//Tipagem dinâmica
+// let nome = 'Mauricio'; //String
+// let idade = 40 ; //number literal
+// let aprovad = true; //boolean
+// let sobrenome = undefined; //Undefined
+// let corSelecionado = null //nulo
+
+// console.log(typeof(nome));
+// console.log(typeof(idade));
+// console.log(typeof(sobrenome));
+// console.log(typeof(aprovad));
+// console.log(typeof(corSelecionado));
+
+//Objetos.
+
+// let pessoa = {
+//     nome: 'Mauricio',
+//     sobrenome: 'Gonzaga Almeida',
+//     idade: 40,
+//     estaAprovado: true,
+// };
+// console.log(pessoa);
+
+//Arrays
+// let familia = [26,45,50,17]
+
+// console.log(familia.length);
+// console.log(familia[2]);
+
+//Functions
+//para nomear uma função precisa de verbo + substantivo
+// let corSite='azul';
+// function resetaCor(cor,tonalidade){
+//     corSite = cor + tonalidade;
+// };
+// console.log(corSite);
+// resetaCor('vermelho',' claro');
+// console.log(corSite);
+
+//Tipos de Funções
+//JavaScript tem dois tipos de funções
+//1° realiza uma tarefa, não devolve nada.
+function dizerNome(){
+    console.log('Mauricio');
+}
+dizerNome();
